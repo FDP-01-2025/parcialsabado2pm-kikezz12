e4d1d504ccb53f9c732e8ba2b699bea05f6020da
gerardo enrique blanco beltran <00208424@uca.edu.sv>
2025-06-07 17:00:00 -0600

    Update ejercicio2.cpp

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main (){
-int numeros[12], numerosinv;
-for (int i = 0; i < 12; i++)
-{
-   cout << "ingrese el numero #" << i + 1 << endl;
-   cin >> numeros[i] ;
-   numerosinv = numeros[i];
-}
+int main() {
+    int numeros[12];
 
+    
+    for (int i = 0; i < 12; i++) {
+        cout << "Ingrese el numero #" << i + 1 << ": ";
+        cin >> numeros[i];
+    }
 
+ 
+    cout << "\nLos numeros en orden inverso son:\n";
+    for (int i = 11; i >= 0; i--) {
+        cout << numeros[i] << " ";
+    }
 
-
-
-
+    cout << endl;
     return 0;
 }
