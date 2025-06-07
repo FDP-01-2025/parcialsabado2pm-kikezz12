f8552da2da9890dc392a4579ea4a4471e0b9578a
kikezz12 <00208424@uca.edu.sv>
2025-06-07 15:36:00 -0600

    Ejercicio 2 de Arrays

---
+++ .vscode/ejercicioextra.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    string nombre;
+    int carnet;
+cout << "por favor ingrse su nombre" << endl;
+cin >> nombre;
+cout << "ingrse su carnet";
+cin >> carnet;
+if (carnet)
+{
+    /* code */
+}
+
+
+
+
+
+
+    return 0;
+}
---
+++ ejercicio2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int numeros[12], numerosinv;
+for (int i = 0; i < 12; i++)
+{
+   cout << "ingrese el numero #" << i + 1 << endl;
+   cin >> numeros[i] ;
+   numerosinv = numeros[i];
+}
+
+
+
+
+
+
+    return 0;
+}
